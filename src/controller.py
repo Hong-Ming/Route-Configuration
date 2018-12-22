50ce4c9805a5f924e18267a0dfa161a458c9e387
Hong-Ming <h15731573@gmail.com>
2018-12-22 19:27:35 +0800

    update controller.py version3

--- src/controller.py
+++ src/controller.py
@@ -119,12 +119,12 @@ class SimpleController1(app_manager.RyuApp):
 
         # Add forwarding rule in s2
         if msg.datapath.id == 2:
-            # For h1-h2 flow: h1 -> s1 -> s3
+            # For h2-h1 flow: h3 -> s2 -> s1
             match = parser.OFPMatch(
                 in_port=2,
                 eth_type=0x0800,
-                ipv4_src="10.0.0.1",
-                ipv4_dst="10.0.0.2",
+                ipv4_src="10.0.0.2",
+                ipv4_dst="10.0.0.1",
                 ip_proto=17,
                 udp_dst=5566)
             actions = [parser.OFPActionOutput(1)]
